6381ce1b20ee40b73c163381466be486c1d58c7c
xhl <2713697048@qq.com>
2022-02-19 11:29:08 +0800

    封装优化 请求工具类

--- common/http.js
+++ common/http.js
@@ -1,5 +1,73 @@
 import qs from 'qs';
 
+/**
+ * 封装用例 
+import Ajax from '@/common/http.js';
+import Api from '@/common/api.js';
+
+import LS from '@/common/utils/localStorage.js';
+
+//请求 发起前 拦截器
+request = Ajax.create();
+request.config.timeout = 16000; //超时时间
+request.interceptors.request = function(config) {
+	//地址设置
+	config.url = (config.baseUrl || Api.baseUrl) + config.url;
+	if (typeof config.showLoadingError != 'boolean')
+		config.showLoadingError = true;
+	//默认 请求头设置
+	let defHeader = {
+		'Content-Type': 'application/json',
+		// 'Authorization': `Basic ${LS.getTokenSync()}`
+		'Authorization': `${LS.getTokenSync()}`
+	};
+	if (config.noToken) { //不需要token
+		delete defHeader['Authorization'];
+	}
+	config.header = Object.assign(defHeader, config.header);
+
+	//是否显示加载提示
+	if (config.showLoading) {
+		uni.showLoading({
+			title: config.loadingText || '加载中',
+			icon: config.loadingIcon || 'loading',
+			mask: config.loadingMask
+		});
+	}
+	return config;
+}
+
+//请求 结果 拦截器
+request.interceptors.response = function(data, resolve, reject) {
+	uni.hideLoading();
+	if (data.statusCode != 200) {
+		reject({
+			code: data.statusCode,
+			msg: "请求失败",
+		});
+		if (data.config.showLoadingError) {
+			uni.showToast({
+				title: `请求失败${data.statusCode}`,
+				icon: 'none'
+			})
+		}
+	} else if ([1, 200].includes(data.data?.code)) {
+		resolve(data.data);
+	} else {
+		if (data.config.showLoadingError) {
+			uni.showToast({
+				title: data.data.msg || data.data.message || `请求失败${data.data.code}`,
+				icon: 'none'
+			})
+		}
+		//delete data.config;
+		reject(data.data);
+	}
+}
+
+module.exports = request;
+
+ */
 function Ajax() {
 	/**
 	 * 拦截器 对象
@@ -47,6 +115,24 @@ Ajax.prototype.request = function(data, abortCall) {
 		});
 	});
 }
+
+// 请求方法
+Ajax.prototype.get = function(data) {
+	data.method = 'GET';
+	return this.request(data);
+}
+Ajax.prototype.post = function(data) {
+	data.method = 'POST';
+	return this.request(data);
+}
+Ajax.prototype.put = function(data) {
+	data.method = 'PUT';
+	return this.request(data);
+}
+Ajax.prototype.del = function(data) {
+	data.method = 'DELETE';
+	return this.request(data);
+}
 /**
  * 请求对象实例
  */
@@ -66,14 +152,17 @@ module.exports.default = ajax;
  * 统一 请求封装
  * @param {Object} data
  *  data 参数说明 {
+		//额外的配置参数 
 		showLoading:,//是否显示加载toast 默认为 false
 		loadingIcon:,//加载提示图标，参考 [https://uniapp.dcloud.io/api/ui/prompt?id=showloading]
 		loadingText:,//加载提示文字 默认为 加载中,
 		loadingMask:,//是否显示透明蒙层，防止触摸穿透，默认：false
 		showLoadingError:,//是否默认显示加载错误信息toast 默认为 true
-		
-		url:,请求地址
 		baseUrl:"",//请求根地址
+		noToken: false,//不需要token
+		
+		//请求参数
+		url:,请求地址
 		method: //请求方式get,post,...
 		data: //body参数
 		query://链接地址上的参数
--- common/request.js
+++ common/request.js
@@ -1,20 +1,24 @@
 import Ajax from '@/common/http.js';
+import Api from '@/common/api.js';
 
 import LS from '@/common/utils/localStorage.js';
-//请求 发起前 拦截器
 
+//请求 发起前 拦截器
 Ajax.config.timeout = 16000; //超时时间
 Ajax.interceptors.request = function(config) {
 	//地址设置
-	config.url = (config.baseUrl || process.env.VUE_APP_BASE_URL) + config.url;
-	// config.url = ('http://192.168.1.110:10010/') + config.url;
+	config.url = (config.baseUrl || Api.baseUrl) + config.url;
 	if (typeof config.showLoadingError != 'boolean')
 		config.showLoadingError = true;
 	//默认 请求头设置
 	let defHeader = {
 		'Content-Type': 'application/json',
-		'Authorization': `${LS.getToken()}`
+		// 'Authorization': `Basic ${LS.getTokenSync()}`
+		'Authorization': `${LS.getTokenSync()}`
 	};
+	if (config.noToken) { //不需要token
+		delete defHeader['Authorization'];
+	}
 	config.header = Object.assign(defHeader, config.header);
 
 	//是否显示加载提示
@@ -30,66 +34,30 @@ Ajax.interceptors.request = function(config) {
 
 //请求 结果 拦截器
 Ajax.interceptors.response = function(data, resolve, reject) {
-	console.log(data.config.url, '==>', data);
 	uni.hideLoading();
-	if (data.statusCode == 200 && data.data.code == 200)
-		resolve(data.data);
-	// else if (data?.data?.code == 401 && data?.data?.msg?.includes('认证失败')) {
-	// 	LS.setToken(null);
-	// 	var pages = getCurrentPages();
-	// 	var page = pages[pages.length - 1];
-	// 	//重新登录
-	// 	uni.navigateTo({
-	// 		url: `/pages/login/index?redirectPage=${encodeURIComponent(page.__page__.fullPath)}`,
-	// 		animationType: 'none'
-	// 	})
-	// } 
-	else if (data.data) {
-		if (data.config.showLoadingError)
+	if (data.statusCode != 200) {
+		reject({
+			code: data.statusCode,
+			msg: "请求失败",
+		});
+		if (data.config.showLoadingError) {
 			uni.showToast({
-				title: data.data.msg || data.data.message || data.data.error || `请求失败${data.data.code}`,
+				title: `请求失败${data.statusCode}`,
 				icon: 'none'
 			})
+		}
+	} else if ([1, 200].includes(data.data?.code)) {
+		resolve(data.data);
+	} else {
+		if (data.config.showLoadingError) {
+			uni.showToast({
+				title: data.data.msg || data.data.message || `请求失败${data.data.code}`,
+				icon: 'none'
+			})
+		}
 		//delete data.config;
 		reject(data.data);
-	} else {
-		reject(data);
 	}
 }
 
-module.exports = {
-	get(data) {
-		data.method = 'GET';
-		return Ajax.request(data);
-	},
-	post(data) {
-		data.method = 'POST';
-		return Ajax.request(data);
-	},
-	put(data) {
-		data.method = 'PUT';
-		return Ajax.request(data);
-	},
-	del(data) {
-		data.method = 'DELETE';
-		return Ajax.request(data);
-	},
-	/**
-	 *  自定义错误处理函数
-	 * @param {String} message : 错误说明
-	 * @param {Boolean} showToast : 是否展示错误提示
-	 * @param {String} toastIcon : 错误提示icon类型：error,none,success
-	 */
-	CustomErrorMessage(message = '参数错误', showToast = false, toastIcon = 'error') {
-		if (showToast) {
-			uni.showToast({
-				title: message,
-				icon: toastIcon
-			})
-		}
-		return Promise.reject({
-			code: 601,
-			message,
-		});
-	}
-}
+module.exports = Ajax;
