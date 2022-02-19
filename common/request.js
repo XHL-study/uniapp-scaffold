437a60dd8f2c0a9bcf96ec037acd9f12c6e039ca
xhl <2713697048@qq.com>
2022-02-19 14:53:19 +0800

    。

--- common/request.js
+++ common/request.js
@@ -3,6 +3,7 @@ import Api from '@/common/api.js';
 
 import LS from '@/common/utils/localStorage.js';
 
+
 //请求 发起前 拦截器
 Ajax.config.timeout = 16000; //超时时间
 Ajax.interceptors.request = function(config) {
@@ -10,17 +11,15 @@ Ajax.interceptors.request = function(config) {
 	config.url = (config.baseUrl || Api.baseUrl) + config.url;
 	if (typeof config.showLoadingError != 'boolean')
 		config.showLoadingError = true;
-	//默认 请求头设置
-	let defHeader = {
+	//请求头配置
+	config.header = Object.assign({
 		'Content-Type': 'application/json',
-		// 'Authorization': `Basic ${LS.getTokenSync()}`
 		'Authorization': `${LS.getTokenSync()}`
-	};
-	if (config.noToken) { //不需要token
-		delete defHeader['Authorization'];
+	}, config.header);
+	//不需要token
+	if (config.noToken) { 
+		delete config.header['Authorization'];
 	}
-	config.header = Object.assign(defHeader, config.header);
-
 	//是否显示加载提示
 	if (config.showLoading) {
 		uni.showLoading({
@@ -35,27 +34,26 @@ Ajax.interceptors.request = function(config) {
 //请求 结果 拦截器
 Ajax.interceptors.response = function(data, resolve, reject) {
 	uni.hideLoading();
-	if (data.statusCode != 200) {
-		reject({
-			code: data.statusCode,
-			msg: "请求失败",
-		});
+	if (data.statusCode != 200) { //请求失败 http状态码不等于 200 时
 		if (data.config.showLoadingError) {
 			uni.showToast({
 				title: `请求失败${data.statusCode}`,
 				icon: 'none'
 			})
 		}
-	} else if ([1, 200].includes(data.data?.code)) {
+		reject({
+			code: data.statusCode,
+			msg: `请求失败`,
+		});
+	} else if ([1, 200].includes(data.data?.code)) { //请求成功 http状态码等于 200 时，接口状态码字段 等于 200|1 时
 		resolve(data.data);
-	} else {
+	} else { //请求成功 http状态码等于 200 时，接口状态码字段 不等于 200 时
 		if (data.config.showLoadingError) {
 			uni.showToast({
 				title: data.data.msg || data.data.message || `请求失败${data.data.code}`,
 				icon: 'none'
 			})
 		}
-		//delete data.config;
 		reject(data.data);
 	}
 }
